df1c80dc87da5e6f3fd22e6a7bc2676d155d0297
kelwin cavalheiro <kelwinkiwi@gmail.com>
2025-11-13 21:02:09 -0300

    primeiro commit

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <ctype.h>
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
@@ -35,6 +35,106 @@ int main() {
     // 0 0 1 0 0
     // 1 1 1 1 1
     // 0 0 1 0 0
+
+    int tamanhoTabuleiro = 10;
+    int tabuleiro[tamanhoTabuleiro][tamanhoTabuleiro];
+    int linha;
+    char coluna;
+    int colunConvertida;
+    int numJogadas = 2;
+    int colunas;
+    char colunasLentras[tamanhoTabuleiro];
+
+    // LINHA: I coluna: J
+    for (int il = 0; il < tamanhoTabuleiro; il++) {
+        for (int ic = 0; ic < tamanhoTabuleiro; ic++) {
+            tabuleiro[il][ic] = 0;   
+        }   
+    }
+    
+    printf("BEM VINDO AO JOGO BATALHA NAVAL: \n");
+    printf("  ");
+    for(int i = 0; i < tamanhoTabuleiro; i++){
+        colunasLentras[i] = 'a'  + i;
+    printf(" %c", colunasLentras[i]);
+    }
+    printf("\n");
+    for (int il = 0; il < tamanhoTabuleiro; il++) {
+        printf("%2d ", il + 1);
+
+        for (int ic = 0; ic < tamanhoTabuleiro; ic++) {
+        
+            printf("%d ", tabuleiro[il][ic]);
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < numJogadas; i++){
+        int validarDoWhile = 0;
+        do{
+            printf("escolha a linha\n");
+            int teste = scanf(" %d", &linha);
+
+            int validar1 = 0;
+            while(validar1 == 0){
+                if (linha > tamanhoTabuleiro || linha < 1 || teste != 1){
+                    printf("Entrada inválida! Digite um número entre 1 e %d.\n", tamanhoTabuleiro);
+                    while(getchar() != '\n');
+                    printf("escolha a linha\n");
+                    teste = scanf(" %d", &linha);
+                }else {
+                    validar1 = 1;
+                }
+            }
+
+            printf("escolha a coluna\n");
+            scanf(" %c", &coluna);
+            int validar = 0;
+            for(int i = 0; i < tamanhoTabuleiro; i++){
+                if(coluna == colunasLentras[i]){
+                    validar = 1;
+                }
+            }
+            while(validar == 0){
+                printf("tem que escolher o valor da coluna\n");
+                for(int i = 0; i < tamanhoTabuleiro; i++){
+                    printf(" %c ",colunasLentras[i]);
+                }
+                printf("\nescolha a coluna\n");
+                scanf(" %c", &coluna);
+                
+                for(int i = 0; i < tamanhoTabuleiro; i++){
+                    if(coluna == colunasLentras[i]){
+                        validar = 1;
+                    }
+                }
+            }
+            
+            colunConvertida = tolower(coluna) - 'a';
+
+            if(tabuleiro[linha - 1][colunConvertida] == 0){
+                tabuleiro[linha - 1][colunConvertida] = 3;
+                validarDoWhile = 1;
+            }else {
+                printf("Você tem que escolher um espaço onde nao foi ocupado ainda\n");
+            }
+        }while(validarDoWhile == 0);
+    }
+
+    printf("NAVIO COLOCADOS \n");
+    printf("  ");
+    for(int i = 0; i < tamanhoTabuleiro; i++){
+        colunasLentras[i] = 'a'  + i;
+    printf(" %c", colunasLentras[i]);
+    }
+    printf("\n");
+    for (int il = 0; il < tamanhoTabuleiro; il++) {
+        printf("%2d ", il + 1);
+        for (int ic = 0; ic < tamanhoTabuleiro; ic++) {
+            printf("%d ", tabuleiro[il][ic]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
